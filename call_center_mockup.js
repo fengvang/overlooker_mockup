556b50a4ee68bf8e36893e6b34819854df84664e
Gluka <38006030+GIuka@users.noreply.github.com>
2022-10-10 17:36:06 -0700

    Make it so mouseHover() only runs if mouse has moved.
    Also got rid of two unnecessary translations and integrated FPS/tileHoverIndex into one function.

--- call_center_mockup.js
+++ call_center_mockup.js
@@ -45,9 +45,10 @@ function layoutA() {
   testDots.colorRandom();
   testDots.display();
 
-  // Debug hud.
+  // Dot logic
   testDots.mouseHover();
-  displayFPS();
+
+  displayDebugHUD();
 }
 
 // P5.js calls this natively every time the window gets resized.
@@ -59,21 +60,18 @@ function windowResized() {
   testDots.updateTilingMaxSpan();
 }
 
-// Prints the current FPS in the upper-left hand corner:
-function displayFPS() {
-  push();
-  fill(color(1, 1, 0));
+// Prints the current FPS and other info in the upper left corner:
+function displayDebugHUD() {
   let fps = Math.round(frameRate());
+  fill(color(1, 1, 0));
 
   // Have to center the text depending on renderer due to different coord systems between P2D and webGL.
   if (boolWGL == 1) {
-    translate(textSpacing - width / 2, (3 * textSpacing - height) / 2, 0);
-    text(fps, 0, 0)
+    text("Index " + testDots.mouseHoverIndex, textSpacing * 2.5 - width / 2, (3 * textSpacing - height) / 2);
+    text(fps, textSpacing - width / 2, (3 * textSpacing - height) / 2)
   } else {
-    translate(textSpacing, (3 * textSpacing) / 2, 0);
-    text(fps, 0, 0)
+    text(fps, (3 * textSpacing) / 2, 0)
   }
-  pop();
 }
 
 // Main dot class meant to represent the person working in the call center:
@@ -116,7 +114,10 @@ class DotGrid {
     // This could be where we first start storing stuff from the API.
     this.dotArray = [];
     this.initDotArray();
-    
+
+    // Stores the index of the dot underneath the mouse.
+    this.mouseHoverIndex = 0;
+
     // Calculate the initial attributes of the grid.
     this.updateTilingMaxSpan();
   }
@@ -209,58 +210,51 @@ class DotGrid {
   // Uses tileSize/gridRows/gridColumns to figure out where the mouse is.
   // BUG: Currently it treats everything like a square.
   // FIX: if the distance between the mouse position and the nearest dot is greater than dotRadius
-  // then we are in empty space (requires one computation of distance formula per frame, so should be cheap).
+  // then we are in empty space (requires one computation of distance formula per call, so should be cheap).
   mouseHover() {
-    let tileIndex = 0;
-    let offsetX = 0;
-    let offsetY = 0;
+    if (mouseX != pmouseX || mouseY != pmouseY) {
 
-    // The mouse position functions also use renderer-dependent coordinates, so we may need 2
-    // different approaches depending on if we're using P2D or WebGL.
-    if (boolWGL == 1) {
+      let offsetX = 0;
+      let offsetY = 0;
 
-      // Offsets needed due to the empty space in the margins.
-      if (this.spanW == 1) {
-        offsetY = (height - this.tileSize * this.gridRows) / 2;
+      // The mouse position functions also use renderer-dependent coordinates, so we may need 2
+      // different approaches depending on if we're using P2D or WebGL.
+      if (boolWGL == 1) {
+        // Offsets needed due to the empty space in the margins.
+        if (this.spanW == 1) {
+          offsetY = (height - this.tileSize * this.gridRows) / 2;
+        } else {
+          offsetX = (width - this.tileSize * this.gridColumns) / 2;
+        }
+        let xPos = mouseX - offsetX;
+        let yPos = mouseY - offsetY;
+        let discreteXPos = floor(xPos / this.tileSize);
+        let discreteYPos = floor(yPos / this.tileSize);
+        this.mouseHoverIndex = discreteXPos + discreteYPos * this.gridColumns;
+
+        // Had trouble finding the offsets to do this, so saving for later.
+        // Could use dot product trick to get out of using sqrt?
+        let centerDistance = sqrt(pow(0, 2) + pow(0, 2));
+
+        if (discreteXPos < 0 || this.gridColumns <= discreteXPos || discreteYPos < 0 || this.dotCount <= this.mouseHoverIndex) {
+          this.mouseHoverIndex = "UDF";
+        } else if (centerDistance > this.tileRadius) {
+          this.mouseHoverIndex = "MISS";
+        }
       } else {
-        offsetX = (width - this.tileSize * this.gridColumns) / 2;
+        // TODO: get it working for P2D as well.
       }
-      let xPos = mouseX - offsetX;
-      let yPos = mouseY - offsetY;
-      let discreteXPos = floor(xPos / this.tileSize);
-      let discreteYPos = floor(yPos / this.tileSize);
-      let tileIndex = discreteXPos + discreteYPos * this.gridColumns;
-
-      // Had trouble finding the offsets to do this, so saving for later.
-      let centerDistance = sqrt(pow(0, 2) + pow(0, 2));
-
-      if (discreteXPos < 0 || this.gridColumns <= discreteXPos || discreteYPos < 0 || this.dotCount <= tileIndex) {
-        tileIndex = "UDF";
-      } else if (centerDistance > this.tileRadius) {
-        tileIndex = "MISS";
-      }
-
-      push();
-      fill(color(1, 1, 0));
-      translate(textSpacing * 2.5 - width / 2, (3 * textSpacing - height) / 2, 0);
-      text("Index " + tileIndex, 0, 0)
-        pop();
-    } else {
-      // TODO: get it working for P2D as well.
     }
   }
 
   // Main grid display function:
-  // Instead of storing all positions in dotSingle[] and iterating thru them, I think we should
-  // just calculate them every frame since it is less code and doesn't seem to perform any worse.
+  // Calculates all dot positions every frame (doesn't seem more expensive than accessing from an array).
   // This same approach seems like it could be used in a shader so it might be good to work with it.
-  // It also adjusts to changes in dotCount/width/height without needing to update an array.
   display() {
     push();
     translate(this.gridOffsetX, this.gridOffsetY);
-    let liveY = 0;
-    let liveX = 0;
-    let tempRadius = this.tileSize - this.dotPadding * this.tileSize;
+    let scanX = 0;
+    let scanY = 0;
     let counter = 0;
 
     // Works like a scanline going from left to right and top to bottom.
@@ -268,23 +262,22 @@ class DotGrid {
       for (let x = 0; x < this.gridColumns; x++) {
         if (counter < this.dotCount) {
           fill(this.dotArray[counter].dotColor);
-          circle(liveX, liveY, tempRadius);
-          liveX += this.tileSize;
+          circle(scanX, scanY, this.dotRadius);
+          scanX += this.tileSize;
           counter++;
         } else {
-
           // Once it hits dotCount it uses a while loop to display all of the grey dots; it breaks out
           // of the loop once the grey dots touch the right side of the screen.
           fill(this.disabledDotColor);
-          while (liveX < this.gridColumns * this.tileSize - 0.001) {
-            circle(liveX, liveY, tempRadius);
-            liveX += this.tileSize;
+          while (scanX < this.gridColumns * this.tileSize - 0.001) {
+            circle(scanX, scanY, this.dotRadius);
+            scanX += this.tileSize;
           }
           break;
         }
       }
-      liveX = 0;
-      liveY += this.tileSize;
+      scanX = 0;
+      scanY += this.tileSize;
     }
     pop();
   }
